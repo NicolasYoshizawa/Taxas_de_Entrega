68a4c06a7d05fb9bee772c7db72cd35667dc8c33
Nicolas Yoshizawa Fumani <120747859+NicolasYoshizawa@users.noreply.github.com>
2023-01-06 18:51:57 -0300

    Update Taxas_Entrega.py
    Adicionando as explicações

--- Taxas_Entrega.py
+++ Taxas_Entrega.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tkinter import *
 import tkinter.font as tkFont
 
+# Cria a função responsável por obter a variável do usuário, buscar o valor correspondente na base de dados e exibe na interface gráfica
 def buscar():
     quant = 0
     error = 0
@@ -25,29 +26,34 @@ def buscar():
         c += 1
     entry.delete(0, END)
 
-
+# Lê a base de dados em excel
 db = pd.read_excel("Taxas de entrega.xlsx", engine='openpyxl')
 
+# Cria e configura a interface gráfica
 root = Tk()
 root.title('Taxas')
 root.geometry('500x250')
 root.resizable(False, False)
 
+# Algumas variáveis e configurações de estilo da interface gráfica
 background = '#696969'
 background2 = '#D3D3D3'
 
 root.configure(bg=background)
 fontStyle = tkFont.Font(family="Comic Sans MS", size=13)
 
+# Cria a entreda para receber a variável do usuário
 entry = Entry(root, bd='5', bg=background2)
 entry.pack(padx=10, pady=10)
 
+# Cria o botão para executar a função
 botao = Button(root, text='Buscar', bd='5', command=buscar, height=2, width=10, bg=background2)
 botao.pack(padx=10, pady=10)
 
+# Configuração para que texto apareça na interface gráfica
 texto1 = Label(root, text='', font=fontStyle, bg=background, foreground='white')
 texto1.pack()
 
-
+# Cria um loop da interface gráfica, mantendo-a aberta
 root.mainloop()
 
